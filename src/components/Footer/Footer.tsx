2743d2086a6ffee91866bea5c1994bff5daf2063
KarimMagdy96 <karimmagdy096@gmail.com>
2024-08-28 00:55:48 +0300

    up

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -6,7 +6,7 @@ interface FooterProps {}
 const Footer: FC<FooterProps> = () => (
   <footer className="bg-white rounded-lg   m-4">
     <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
-      <div className="sm:flex sm:items-center sm:justify-between">
+      <div className="flex justify-between">
         <Link
           to="/"
           className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse"
