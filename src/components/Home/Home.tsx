5354a0e17ec7490d510deeb6d03d9e4d487f8465
KarimMagdy96 <karimmagdy096@gmail.com>
2024-09-01 17:43:23 +0300

    up

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,14 +1,22 @@
-import { FC } from "react";
+import { FC, useContext, useEffect } from "react";
 import RecentProducts from "../RecentProducts/RecentProducts";
 import CategorySlider from "../CategorySlider/CategorySlider";
 import MainSlider from "../MainSlider/MainSlider";
 import useRequist from "../../Hooks/useRequist";
 import { MoonLoader } from "react-spinners";
 import { Helmet } from "react-helmet";
+import { cartContext } from "../../Context/CartContext";
 interface HomeProps {}
 const Home: FC<HomeProps> = () => {
   let { isLoading } = useRequist();
-
+  let { getLoggedUserCart } = useContext(cartContext);
+  useEffect(() => {
+    if (localStorage.getItem("userToken") !== null) {
+      getLoggedUserCart().then((response: any) => {
+        localStorage.setItem("cartId", response?.data?.data?.cartOwner);
+      });
+    }
+  }, []);
   if (isLoading) {
     return (
       <div className=" w-full h-screen flex justify-center items-center ">
